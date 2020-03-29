f3f9a852211c8e3fb672d39e0ef8e958dcce0714
Heewon Ahn <57425238+shelost@users.noreply.github.com>
2020-03-29 16:42:50 -0400

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,23 @@
+var W = {
+
+    'CANADA': {
+        date: '1901 ~ Present',
+        info: 'A former British colony, Canada is now the geographically largest member of the British Commonwealth.'
+    },
+    'UNITED STATES OF AMERICA': {
+        date: '1789 ~ Present',
+        info: 'The most powerful nation in the history of our planet, the United States has been the world\'s sole global superpower ever since the demise of its Cold War rival, the USSR, in the early 1990s.'
+    },
+    'SOUTH KOREA': {
+        date: '1948 ~ Present',
+        info: 'After decades of Japanese occupation and a bloody separation from its northern neighbor, South Korea now boasts the 11th largest economy in the world and a growing global cultural presence.'
+    },
+    'NORTH KOREA': {
+        date: '1948 ~ Present',
+        info: 'The Kim dynasty has ruled the reclusive North Korea since its inception, and its possession of nuclear weapons contributes to East Asia\'s tense political climate.'
+    },
+    'CHINA': {
+        date: '1949 ~ Present',
+        info: 'The Chinese Communist Party, after taking control during the course of a bloody civil war in the first half of the twentieth century, has led China in a one-party arrangement since the nation\'s inception.'
+    },
+}
